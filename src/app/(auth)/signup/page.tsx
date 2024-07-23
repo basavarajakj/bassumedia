afc2843fe62e01847b0cb029f7560efc7acb8e5b
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-07-23 22:54:08 +0530

    Sign up page creation and planing 🤔🤔

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="max-h[40rem] flex h-full w-full max-w-[40rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+        <div className=" w-full space-y-10 overflow-y-auto p-10">
+          <div className="space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign up to BassuMedia</h1>
+            <p className="text-muted-foreground">
+              A place where even <span className="italic">you</span> can find a friend.
+            </p>
+          </div>
+          <div className="space-y-5">
+
+            <Link href="login" className="block text-center hover:underline">
+              Already have an account? Log in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
